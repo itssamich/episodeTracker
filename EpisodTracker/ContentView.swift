943079cb10678c83b864ca8df7319558c5bbf63c
itssamich <alex.klein4414@gmail.com>
2022-08-12 13:26:29 -0600

    Initial Commit

---
+++ EpisodTracker/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  EpisodTracker
+//
+//  Created by Alex Klein on 8/12/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ EpisodTracker/EpisodTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  EpisodTrackerApp.swift
+//  EpisodTracker
+//
+//  Created by Alex Klein on 8/12/22.
+//
+
+import SwiftUI
+
+@main
+struct EpisodTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
